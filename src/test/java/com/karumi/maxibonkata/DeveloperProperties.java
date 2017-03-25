f7ca403b493d765a68ea662451e56146dfabde7a
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-03-25 12:05:43 +0100

    Added unit test

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -3,8 +3,12 @@ package com.karumi.maxibonkata;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -18,8 +22,23 @@ public class DeveloperProperties {
     private static final String FAKE_NAME = "String";
 
     @Property public void theNumberOfMaxiAreAlwaysPositiveOrZero(int numOfMaxi) {
+        System.out.println("num of maxi generated ="+numOfMaxi);
         Developer developer = new Developer(FAKE_NAME, numOfMaxi);
         System.out.println(developer);
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
     }
+
+    @Property
+    public void theNameOfADeveloperGenerator(String name) {
+        //System.out.println(name);
+        Developer developer = new Developer(name, 1);
+        assertThat(developer, is(notNullValue()));
+    }
+
+    @Test
+    public void pedroDeveloperHasThreeMaxis() {
+        Developer pedro = Karumies.PEDRO;
+        assertThat(pedro.getNumberOfMaxibonsToGrab(), is(3));
+    }
+
 }
