b0dc32f5980c8fafec9c39a1a65620443af1d37c
Pol Gómez Guerrero <thebestpol@gmail.com>
2017-03-25 13:11:36 +0100

    Mocking chat and verify method call

--- src/test/java/com/karumi/maxibonkata/KarumiHQTest.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQTest.java
@@ -6,8 +6,13 @@ import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
 import org.junit.runner.RunWith;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by PoL on 25/03/17.
@@ -28,13 +33,37 @@ public class KarumiHQTest {
     }
 
     @Property
-    public void karumiHQAlwaysMustHaveMoreThanTwoWithStrangePeaple(
+    public void karumiHQAlwaysMustHaveMoreThanTwoWithStrangePeople(
             @From(DevelopersGenerator.class) Developer developer) {
         System.out.println(developer);
 
         hQs.openFridge(developer);
 
         assertThat(hQs.getMaxibonsLeft() > 2, is(true));
+    }
+
+    // This kind of method is not supported in Android development due the annotation inside List<>
+    @Property
+    public void karumiHQAlwaysMustHaveMoreThanTwoWithListOfPeople(
+            List<@From(DevelopersGenerator.class) Developer> developer) {
+        System.out.println(developer);
+
+        hQs.openFridge(developer);
+
+        assertThat(hQs.getMaxibonsLeft() > 2, is(true));
+    }
+
+    @Property
+    public void shouldCallChatIfMaxisAreLowerThanTwo(
+            @From(HungryDevelopersGenerator.class)Developer hungryDeveloper) {
+
+        Chat mockChat = mock(Chat.class);
+        KarumiHQs karumiHQs = new KarumiHQs(mockChat);
+
+        karumiHQs.openFridge(hungryDeveloper);
+
+        verify(mockChat).sendMessage(anyString());
+
     }
 
 
